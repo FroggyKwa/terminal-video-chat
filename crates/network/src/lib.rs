5da1a986bb1bee202f8cb77db7e3a842f3eb932f
FroggyKwa <ilya.laguxa@gmail.com>
2026-03-18 23:45:08 +1000

    project skeleton

---
+++ crates/network/src/lib.rs
@@ -0,0 +1,5 @@
+#[derive(Debug, Clone, Default)]
+pub struct SignalingClient;
+
+#[derive(Debug, Clone, Default)]
+pub struct PeerConnection;
---
+++ frontends/tui/src/lib.rs
@@ -0,0 +1,4 @@
+pub fn run() -> i32 {
+    println!("frontend: tui");
+    0
+}
